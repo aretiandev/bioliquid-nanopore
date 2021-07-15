7dc7dbe7452d1602a541b310ad87afa87ab6f3ea
yufernando <yufernando@gmail.com>
2021-07-15 16:39:03 -0400

    09_boolean_matrix: create R script and add to Makefile

---
+++ 09_boolean_matrix.R
@@ -0,0 +1,75 @@
+# 09 - Create Boolean Matrix
+#
+# This scripts creates a matrix of boolean values per read based on str presence. Example:
+# 
+#  person-read   str1  str2  str3  str4  str5
+#  p0-01         0     1     0     1     0
+#  p0-02         1     0     0     1     0
+#  p1-01         1     0     0     1     0
+#
+# INPUTS:
+#   run_number
+#   disease
+#   nanopore reads for location: e.g. run1_chr11_sca.sam
+#   reference genome fasta for location: e.g. chr11_selected.fa
+# 
+# OUTPUTS:
+#   reads without gaps: e.g. run1_chr11_sca_clean.csv
+#   reference genome without gaps: e.g. run1_chr11_reference_genome.json
+print('')
+print('09 - BOOLEAN MATRIX')
+print('')
+
+# Load Modules
+# -----------------------------------------------------------------------------
+print('Loading Modules.')
+suppressMessages(library(dplyr))
+suppressMessages(library(reshape2))
+
+# Set Parameters
+# -----------------------------------------------------------------------------
+# print('Setting parameters.')
+args = commandArgs(trailingOnly=TRUE)
+
+# Setup
+run_num = args[1]
+dis = args[2]
+print(paste0("Run: ", run_num, ", disease: ", dis))
+
+run_number=paste0("run",run_num)
+
+dis_params = data.frame('disease' = c('sca', 'cystic', 'spinal1', 'spinal2', 'thal1', 'thal2', 'thal3', 'pompe'),
+                    'chr' = c('chr11','chr7','chr5', 'chr5', 'chr16', 'chr16', 'chr16', 'chr17'),
+                    'start' = c(5227002, 117559590, 70924941, 70049523, 176680, 172876, 5225464, 0),
+                    'end'   = c(5227002, 117559590, 70953015, 70077595, 177522, 173710, 5227071, 0))
+
+chr = dis_params %>% filter(disease == dis) %>% select(chr) %>% pull
+
+datadir = paste0('/mnt/aretian/genomics/nanopore/',run_number)
+
+# Load Data
+# -----------------------------------------------------------------------------
+print(paste0('Loading tagged reads: ',datadir,'/', run_number,'_',chr,'_tagged_reads.csv'))
+df <- read.csv(paste0(datadir,'/', run_number,'_',chr,'_tagged_reads.csv'), stringsAsFactors = FALSE)
+
+# Helper Functions
+# -----------------------------------------------------------------------------
+# instead of using length (which refers to length of STR) just use 1 to indicate it exists
+agg_func <- function(l){return(1)}
+
+unmeltdf <- function(data) {
+    df <- data %>% mutate(val = 1)
+    df2 <- dcast(df, read_id ~ str_id, fill = 0, value.var = 'val', fun.aggregate = agg_func)
+    return(df2)
+}
+
+# Implementation
+# -----------------------------------------------------------------------------
+print('Creating Boolean Matrix.')
+booldf <- unmeltdf(df)
+
+# Save
+# -----------------------------------------------------------------------------
+write.csv(booldf, paste0(datadir,'/',run_number,'_',chr,'_bool_tagged_reads.csv'), row.names = FALSE)
+
+print(paste0('Saved: ', datadir,'/',run_number,'_',chr,'_bool_tagged_reads.csv'))
--- Makefile
+++ Makefile
@@ -1,4 +1,16 @@
-.PHONY: extract remove_gaps cluster assign create_bams
+# Bioliquid Pipeline
+#
+# Instructions:
+#
+# There are three blocks of code to be run in order:
+#
+#   $make cluster
+#   $make strspy
+#   $make str_cluster
+#
+# Every other step in between will be run automatically. The reason to split the three blocks is that 05_clustering.py and 07_strspy.sh take a long time to run so you might want to avoid those depending on the case
+
+.PHONY: extract remove_gaps cluster assign create_bams strspy_config strspy tag_reads boolean_matrix clustering
 
 extract:
 	@bash 03_extract_location.sh $(run) $(dis)
@@ -13,4 +25,19 @@ assign:
 	@/home/fer/miniconda3/envs/genomics/bin/python3 06_read_assignment.py $(run) $(dis)
 
 create_bams: assign
-	@/home/fer/miniconda3/envs/genomics/bin/python3 06_create_bams.sh $(run) $(dis)
+	@bash 06_create_bams.sh $(run) $(dis)
+
+strspy_config: create_bams
+	@/home/fer/miniconda3/envs/genomics/bin/python3 07_strspy_config.py $(run) $(dis)
+
+strspy: 
+	@bash 07_strspy.sh $(run) $(dis)
+
+tag_reads:
+	@/usr/bin/Rscript 08_tag_reads.R $(run) $(dis)
+
+boolean_matrix: tag_reads
+	@/usr/bin/Rscript 09_boolean_matrix.R $(run) $(dis)
+
+clustering: boolean_matrix
+	@/usr/bin/Rscript 10_clustering.R $(run) $(dis)
