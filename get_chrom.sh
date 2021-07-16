572fbb842b69cdf4172f69e0b0194cc74dae2ef6
yufernando <yufernando@gmail.com>
2021-07-15 20:30:16 -0400

    Add get_chrom script for Makefile

---
+++ get_chrom.sh
@@ -0,0 +1,15 @@
+if [ $1 == 'cf' ]
+then echo 'chr7'
+elif [ $1 == 'sca' ]
+then echo 'chr11'
+elif [ $1 == 'sma' ]
+then echo 'chr5'
+elif [ $1 == 'thal1' ]
+then echo 'chr16'
+elif [ $1 == 'thal2' ]
+then echo 'chr16'
+elif [ $1 == 'thal3' ]
+then echo 'chr16'
+elif [ $1 == 'pompe' ]
+then echo 'chr17'
+fi
