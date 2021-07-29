b3f1220998c43756d65dffd385bac2d7dfdd70b5
yufernando <yufernando@gmail.com>
2021-07-28 21:05:12 -0400

    Makefile bug: get right chrom as input of makefile

--- src/get_chrom.sh
+++ src/get_chrom.sh
@@ -2,14 +2,16 @@ if [ $1 == 'cf' ]
 then echo 'chr7'
 elif [ $1 == 'sca' ]
 then echo 'chr11'
-elif [ $1 == 'sma' ]
+elif [ $1 == 'sma1' ]
+then echo 'chr5'
+elif [ $1 == 'sma2' ]
 then echo 'chr5'
 elif [ $1 == 'thal1' ]
 then echo 'chr16'
 elif [ $1 == 'thal2' ]
 then echo 'chr16'
 elif [ $1 == 'thal3' ]
-then echo 'chr16'
+then echo 'chr11'
 elif [ $1 == 'pompe' ]
 then echo 'chr17'
 fi
